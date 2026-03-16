917ffb3d8a950c0089f4ddae7a750cfb9dc33c43
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-03-16 16:11:03 -0300

    .
    .

--- carregar_twig.php
+++ carregar_twig.php
@@ -1,5 +1,5 @@
 <?php
 require_once("vendor/autoload.php");
 
-$loader = new \Twig\Loader\FilesystemLoader('templates');
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__.'/templates');
 $twig = new \Twig\Environment($loader);
