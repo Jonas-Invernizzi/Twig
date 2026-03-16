047c6253e7faf56721f42daf83ff1c736b651f03
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-03-16 16:23:17 -0300

    .
    .

--- teste_twig.php
+++ teste_twig.php
@@ -4,7 +4,15 @@ require_once('carregar_twig.php');
 
 $nome = 'fulaninho';
 
+$disciplinas = [
+    'Programação',
+    'Banco de Dados',
+    'Interface Web',
+    'Desenvolvimento de Sistemas',
+];
+
 echo $twig->render('teste_twig.html',[
     'nome'=>$nome,
     'legal'=>true,
+    'disciplinas'=>$disciplinas,
 ]);
