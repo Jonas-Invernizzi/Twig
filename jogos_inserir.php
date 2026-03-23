1db4f3c64013ba4f3078dd0d166661b85c7fe5dc
Jonas-Invernizzi <jonasinvernizzibg14@gmail.com>
2026-03-23 17:20:08 -0300

    .
    .

--- jogos_inserir.php
+++ jogos_inserir.php
@@ -1,15 +1,31 @@
 <?php
 $erro = false;
-if ($_SERVER["REQUEST_METHOD"] == "POST" ){
+$nome = false;
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST['nome'] ?? false;
     $estilo = $_POST['estilo'] ?? false;
-}
-if(!$nome || !$estilo){
-    $erro = "⚠️Preencha todos os campo.";
-}
-else{
     
 }
+if ((!$nome || !$estilo)) {
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $erro = "⚠️Preencha todos os campo.";
+    }
+} else {
+    $ext = pathinfo($_FILES["capa"]["name"], PATHINFO_EXTENSION);
+    $capa = uniqid().'.';
+
+    require("carregar_pdo.php");
+    $dados = $pdo->prepare('INSERT INTO jogos (nome, estilo) VALUES (?,?)');
+    $dados->bindParam(1, $nome);
+    $dados->bindParam(2, $estilo);
+    
+    $dados->execute();
+
+    header('location:jogos.php');
+    die;
+}
+
 require('carregar_twig.php');
 
 echo $twig->render('jogos_inserir.html', [
